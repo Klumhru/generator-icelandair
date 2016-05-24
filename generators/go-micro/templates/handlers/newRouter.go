fcead7322d7762f943637d06d49b014ad3d879aa
garpur <garpur@kolibri.is>
2016-05-24 20:22:33 +0000

    added an example using a mock upstream server

--- generators/go-micro/templates/handlers/newRouter.go
+++ generators/go-micro/templates/handlers/newRouter.go
@@ -3,9 +3,9 @@ package handlers
 import "github.com/gorilla/mux"
 
 // NewRouter returns a new router for the service
-func NewRouter() *mux.Router {
+func NewRouter(upstreamProviderURL string) *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
-	contentItem := NewContentItemContext()
+	contentItem := NewContentItemContext(upstreamProviderURL)
 
 	router.Methods("GET").Path("/item").HandlerFunc(contentItem.Handle)
 	router.Methods("GET").Path("/error").HandlerFunc(contentItem.Generate500Error)

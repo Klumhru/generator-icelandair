813d9b45abfad7af40f52fb4c057ee96d4b68d0e
Súsanna Þorvaldsdóttir <susanna@tmsoftware.is>
2016-09-22 11:01:06 +0000

    Adding Logger to generator.

--- generators/go-micro/templates/handlers/new_router.go
+++ generators/go-micro/templates/handlers/new_router.go
@@ -1,21 +1,23 @@
 package handlers
 
 import (
-	baseHandlers "github.com/Icelandair/go.base/handlers"
-	"github.com/gorilla/mux"
+  baseHandlers "github.com/Icelandair/go.base/handlers"
+  "github.com/Icelandair/<%= gitRepo %>/db"
+  "github.com/gorilla/mux"
 )
 
 // NewRouter returns a new router for the service
-func NewRouter(upstreamProviderURL string) *mux.Router {
+func NewRouter(upstreamProviderURL string, name string, runtimeEnvironment string, logger baseHandlers.Logger, fetcher db.SomeDBModelAccesser) *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
-	contentItem := NewContentItemContext(upstreamProviderURL)
-
-	router.Methods("GET").Path("//api/{id:v[0-9]+}/").HandlerFunc(contentItem.Handle)
-	router.Methods("GET").Path("/item/").HandlerFunc(contentItem.Handle)
-	router.Methods("GET").Path("/error/").HandlerFunc(contentItem.Generate500Error)
+	contentItem := NewContentItemContext(upstreamProviderURL string, name, runtimeEnvironment, logger, fetcher)
 
 	health := baseHandlers.NewHealthContext()
-	router.HandleFunc("/health/", health.Handle)
+	router.HandleFunc("/api/v1/<%= camelProjectName %>/health/", health.Handle)
+
+	router.Methods("GET").Path("/api/{id:v[0-9]+}/<%= camelProjectName %>/item/").HandlerFunc(contentItem.Handle)
+	router.Methods("GET").Path("/api/{id:v[0-9]+}/<%= camelProjectName %>/error/").HandlerFunc(contentItem.Generate500Error)
+	router.Methods("GET").Path("/api/{id:v[0-9]+}/<%= camelProjectName %>/").HandlerFunc(contentItem.Handle)
+
 
 	return router
 }
--- generators/go-micro/templates/main.go
+++ generators/go-micro/templates/main.go
@@ -6,29 +6,43 @@ import (
 	"os"
 	"time"
 
-	"github.com/Icelandair/go.base/middleware"
-	"github.com/<%= gitRepo %>/handlers"
+  baseMiddleware "github.com/Icelandair/go.base/middleware"
+	"github.com/<%= gitRepo %>/db"
+  "github.com/<%= gitRepo %>/handlers"
+  "github.com/<%= gitRepo %>/utils"
 	"github.com/codegangsta/negroni"
-
+  _ "github.com/lib/pq" // Postgres driver
 	"github.com/tylerb/graceful"
 )
 
-var (
-	connString string
-)
-
 func main() {
-	n := negroni.New(negroni.NewRecovery())
-	log.Println("Initializing service")
+  serviceName := "<%= camelProjectName %>"
+  // todo if need for db, else remove
+  fetcher := &db.SomeDBModel{
+    Database: db.InitDB(),
+  }
 
-	mux := handlers.NewRouter("some url to an actual service")
-  n.Use(middleware.NewLogger("<%= camelProjectName %>"))
-  n.Use(middleware.NewCorrelationID())
-  //n.Use(middleware.NewRequiredLanguageID())
-  //n.Use(middleware.NewRequiredLocationID())
+  runtimeEnvironment := fmt.Sprintf("%s", os.Getenv("RUNTIME_ENVIRONMENT"))
+  logger := baseMiddleware.NewLogger(serviceName, runtimeEnvironment)
+
+  n := negroni.New(negroni.NewRecovery())
+  // todo add or remove client to e.g. webservice
+  mux := handlers.NewRouter("Url to client e.g. http://www.icelandair.is/servlet/rest/labs", serviceName, runtimeEnvironment, *logger, fetcher)
+  n.Use(logger)
+
+  cid := baseMiddleware.NewRequiredCorrelationID(serviceName, runtimeEnvironment)
+  corrid, _ := cid.GenerateID()
+  n.Use(cid)
+
+  //.Use(baseMiddleware.NewRequiredLanguageID())
+
   n.UseHandler(mux)
 
-	host := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
-	log.Printf("Starting service on %s\n", host)
-	graceful.Run(host, 10*time.Second, n)
+  host := fmt.Sprintf("%s:%s", os.Getenv("HOSTNAME"), os.Getenv("PORT"))
+  nodeName := fmt.Sprintf("%s", os.Getenv("HOSTNAME"))
+  message := fmt.Sprintf("Starting service on %s\n", host)
+  fields := utils.GetServerMessage(nodeName, serviceName, "main", runtimeEnvironment)
+  logger.Logger.WithFields(fields).Info(message)
+  logger.Logger.WithFields(fields).Info("Using baseMiddleware.NewRequiredCorrelationID: %v\n", corrid)
+  graceful.Run(host, 10*time.Second, n)
 }
